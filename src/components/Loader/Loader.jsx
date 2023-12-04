617f23bb32b473ae8f68ca3468e9a800b0e40bd1
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-04 01:23:52 +0100

    Change loader styling

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -9,9 +9,9 @@ export default class Loader extends Component {
         height="80"
         width="80"
         radius="9"
-        color="#4fa94d"
+        color="#3f51b5"
         ariaLabel="three-dots-loading"
-        wrapperStyle={{}}
+        wrapperStyle={{ justifySelf: 'center' }}
         wrapperClassName=""
         visible={true}
       />
